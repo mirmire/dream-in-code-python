9b5e6572813c92f1808280832fcd6f64789a2fff
Prakash Acharya <33595598+mirmire@users.noreply.github.com>
2018-03-19 20:27:12 +0200

    modified the program to take file as an input

--- words_count.py
+++ words_count.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 # This program counts the number of words in a given sentence.
+words = []
+counted_words = []
 
 
-def main():
-    words = []
-    counted_words = []
-    string = input("Number of words in: ")
+def count_words(string):
     for word in string.split():
         words.append(word)
 
@@ -17,5 +16,18 @@ def main():
             counted_words.append(each_word)
 
 
+def main():
+    filepath = input("Write the absolute path of the filename: ")
+
+    try:
+        with open(filepath, "r") as f:
+            count_words(f.read())
+    except (FileNotFoundError, IsADirectoryError):
+        print("File does not exist or not a valid file name.")
+        return
+
+
 if __name__ == '__main__':
+    print("\nWith this program, you can count number of words and occurences\n"
+          "of each word in user specified file.\n")
     main()
